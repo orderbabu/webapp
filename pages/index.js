1cb38daae9dff95147560b83a44ba0e12f54145a
Nandakishore Bhandari <nandakishorebhandari@gmail.com>
2020-09-27 17:13:30 +0530

    add sites

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,76 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useEffect } from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../src/ProTip';
+import Link from '../src/Link';
+import Copyright from '../src/Copyright';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-export default function Home() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Index() {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.text = `amzn_assoc_ad_type ="responsive_search_widget"; amzn_assoc_tracking_id ="orderbabu-21"; amzn_assoc_marketplace ="amazon"; amzn_assoc_region ="IN"; amzn_assoc_placement =""; amzn_assoc_search_type = "search_widget";amzn_assoc_width ="300"; amzn_assoc_height ="250"; amzn_assoc_default_search_category =""; amzn_assoc_default_search_key ="Mobile";amzn_assoc_theme ="dark"; amzn_assoc_bg_color ="000000";`
+
+    const script2 = document.createElement('script')
+    script2.src = '//z-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=IN'
+    script2.async = true
+
+    const amazonAdd = document.querySelector('#amzn_assoc_ad_div_assoc_banner_placement_default_0')
+
+
+    const script3 = document.createElement('script')
+    script3.src = `//affiliate.flipkart.com/affiliate/widgets/FKAffiliateWidgets.js`
+    script3.async = true
+
+    const flpkartAdd = document.querySelector('#flpkart-id-1')
+
+
+    if (amazonAdd) {
+      amazonAdd.appendChild(script)
+      amazonAdd.appendChild(script2)
+    }
+    // if (flpkartAdd) {
+    flpkartAdd.appendChild(script3)
+    // }
+  }, [])
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Container maxWidth="sm">
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} lg={8}  >
+          <Typography variant="h4" component="h2" gutterBottom>
+            Order Babu
+      </Typography>
+        </Grid>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Grid item xs={12} lg={8}  >
+          <div id="flpkart-id-1" >
+            <div data-WRID="WRID-160120568436633865" data-widgetType="searchWidget" data-class="affiliateAdsByFlipkart" height="250" width="300" ></div>
+          </div>
+        </Grid>
+        <Grid item xs={12} lg={8}  >
+          <div id="amzn_assoc_ad_div_assoc_banner_placement_default_0" />
+        </Grid>
+      </Grid>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    </Container>
+  );
 }
